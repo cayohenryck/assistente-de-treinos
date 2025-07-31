2fac861b048312a42b52d3ff0e692c2b84de82dd
Cayo Maia <chenrycksm@gmail.com>
2025-07-31 01:25:54 -0300

    feat: persist workout list in localStorage and add remove button support.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,35 @@ const addWorkoutForm = document.getElementById('add-workout-form');
 const cancelBtn = document.getElementById('cancel-btn');
 const workoutNameInput = document.getElementById('workout-name-input');
 const workoutDescInput = document.getElementById('workout-desc-input');
+const workoutContainer = document.getElementById('workout-container');
+
+
+addWorkoutForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const workoutName = workoutNameInput.value.trim();
+    const workoutDesc = workoutDescInput.value.trim();
+
+    if (!workoutName) {
+        alert("O nome do treino é obrigatório!");
+        return;
+    }
+
+    console.log("Treino que vai ser adicionado:");
+    console.log({ name: workoutName, description: workoutDesc });
+
+    const workoutToAdd = {
+        title: workoutNameInput.value,
+        description: workoutDescInput.value,
+    };
+
+    workouts.push(workoutToAdd);
+    renderWorkout();
+    saveWorkout();
+
+    addWorkoutForm.reset();
+    closeModal();
+});
 
 function openModal() {
     modalOverlay.style.display = 'flex';
@@ -24,20 +53,53 @@ modalOverlay.addEventListener('click', (event) => {
     }
 });
 
-addWorkoutForm.addEventListener('submit', (event) => {
-    event.preventDefault();
+const defaultWorkout = [
+    {
+    title: "Braços - Segunda feira",
+    description: "Rosca direta (3x12), rosca martelo (3x10-12), rosca concentrada unilateral (3x10), tríceps testa com halteres (3x12), tríceps coice (3x10-12) e mergulho no banco (3x10-15).",
+    },
+];
 
-    const workoutName = workoutNameInput.value.trim();
-    const workoutDesc = workoutDescInput.value.trim();
+const savedWorkouts = localStorage.getItem('workouts-saved');
 
-    if (!workoutName) {
-        alert("O nome do treino é obrigatório!");
-        return;
+let workouts = savedWorkouts ? JSON.parse(savedWorkouts) : defaultWorkout;
+
+function saveWorkout() {
+    localStorage.setItem('workouts-saveds', JSON.stringify(workouts));
+}
+
+function renderWorkout(){
+    const workoutCardsHTML = workouts.map(workout => {
+        return `<div class="workout-card">
+                    <h4 class="card-title title">${workout.title}</h3>
+                    <div class="info-repetition-container item-individual-container">
+                        <!-- <h5 class="exercises-number-title title">NÚMERO DE EXERCICIOS: </h5>
+                         <p class="exercises-number desc">5</p>-->
+                    </div>
+                    <div class="description-container item-individual-container">
+                        <p class="desc">${workout.description}</p>
+                        <button class="remove-btn" data-title="${workout.title}">X</button>
+                    </div>
+                </div>
+                `
+    });
+
+    const containerHTML = workoutCardsHTML.join('');
+    workoutContainer.innerHTML = containerHTML;
+};
+
+workoutContainer.addEventListener('click', (event) => {
+    const clickElement = event.target;
+    const removeButton = clickElement.closest('.remove-btn');
+
+    if (removeButton){
+        const workoutToRemove = removeButton.dataset.title;
+
+        workouts = workouts.filter(workout => workout.title !== workoutToRemove);
+        
+        renderWorkout();
+        saveWorkout();
     }
+})
 
-    console.log("Treino que vai ser adicionado:");
-    console.log({ name: workoutName, description: workoutDesc });
-
-    addWorkoutForm.reset();
-    closeModal();
-});
+renderWorkout();
